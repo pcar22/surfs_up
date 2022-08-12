0c9c512f0ebb2d92fc6936a36061ed79e4282bd5
pcar22 <pcarranza2@comcast.net>
2022-08-12 16:02:08 -0600

    updates

--- app.py
+++ app.py
@@ -6,27 +6,22 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
-#################################################
-# Database Setup
-#################################################
-# engine = create_engine("sqlite:///hawaii.sqlite")
+
+
 engine = create_engine("sqlite:///surfs_up\hawaii.sqlite")
-# reflect an existing database into a new model
+
 Base = automap_base()
-# reflect the tables
+
 Base.prepare(engine, reflect=True)
-# Save references to each table
+
 Measurement = Base.classes.measurement
 Station = Base.classes.station
-# Create our session (link) from Python to the DB
+
 session = Session(engine)
-#################################################
-# Flask Setup
-#################################################
+
+
 app = Flask(__name__)
-#################################################
-# Flask Routes
-#################################################
+
 @app.route("/")
 def welcome():
     test = (f"Welcome to the Hawaii Climate Analysis API!<br/>"
